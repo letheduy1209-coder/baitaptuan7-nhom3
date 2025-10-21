78c23028593f6547480e1d1cb42b9540c3edac46
tcson5932-crypto <tcson5932@gmail.com>
2025-10-21 22:08:09 +0700

    Bai tap tuan 7

--- tuan7__/IKiemKe.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * Interface kiểm kê: kiemTraTonKho và capNhatViTri
- */
-public interface IKiemKe {
-    boolean kiemTraTonKho(int soLuongToiThieu);
-    void capNhatViTri(String viTriMoi);
-}
