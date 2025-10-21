ad0b8cc2c0336baf3ced7df7e21504e21ac89f7d
Ngyuen Bao Phuc <phucnguyen2k20@gmail.com>
2025-10-21 19:13:31 +0700

    bài tập tuần 5

--- tuan5/Test.java
+++ tuan5/Test.java
@@ -1,18 +1,15 @@
+package tuan5;
 public class Test {
     public static void main(String[] args) {
-        // Tao sach bang constructor
-        Sach sach1 = new Sach("B001", "Lap trinh Java", "Nguyen Van A", 2022, 10);
+        Sach laptrinh = new Sach("B001", "Lập trình Java", "Nguyễn Văn A", 2021, 10);
+        Sach ctdlgt = new Sach();
+        ctdlgt.setMaSach("B002");
+        ctdlgt.setTieuDe("Cấu trúc dữ liệu");
+        ctdlgt.setTacGia("Trần Thị B");
+        ctdlgt.setNamXuatBan(2020);
+        ctdlgt.setSoLuong(5);
 
-        // Tao sach bang setter
-        Sach sach2 = new Sach();
-        sach2.setMaSach("B002");
-        sach2.setTieuDe("Cau truc du lieu");
-        sach2.setTacGia("Tran Van B");
-        sach2.setNamXuatBan(2021);
-        sach2.setSoLuong(5);
-
-        // Hien thi thong tin
-        sach1.hienThiThongTin();
-        sach2.hienThiThongTin();
+        laptrinh.hienThiThongTin();
+        ctdlgt.hienThiThongTin();
     }
 }
